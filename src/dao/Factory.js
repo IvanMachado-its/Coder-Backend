03d354268f173c2cac4a885ed5e961571803cf54
IvanMachado-its <64422033+IvanMachado-its@users.noreply.github.com>
2024-08-29 13:57:39 -0300

    Delete src directory

--- src/dao/Factory.js
+++
@@ -1,9 +0,0 @@
-const MongoDBDAO = require('./MongoDBDAO');
-
-class DAOFactory {
-  static getDAO() {
-    return MongoDBDAO;
-  }
-}
-
-module.exports = DAOFactory;
--- src/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const logger = require('../config/logger');
-
-module.exports = (err, req, res, next) => {
-  logger.error(`Error: ${err.message}`);
-  res.status(500).send('Something broke!');
-};
