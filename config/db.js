d6423591779d55d5d6d858995fa4331ca9f9d272
Ivan Trabajo <ivanmachado146@gmail.com>
2024-09-03 10:21:59 -0300

    Entrega

--- config/db.js
+++ config/db.js
@@ -5,12 +5,10 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: true,
-            useCreateIndex: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
